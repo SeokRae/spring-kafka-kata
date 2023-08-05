1672008d6f499cca8ec35dd8a119ed669bcfd6b4
SeokRae <kslbsh@gmail.com>
2023-08-05 19:20:27 +0900

    - [x] 기본 세팅

--- producer-api/src/main/java/com/example/producer/ProducerApiApplication.java
+++ producer-api/src/main/java/com/example/producer/ProducerApiApplication.java
@@ -2,8 +2,10 @@ package com.example.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.example.producer.core.props")
 public class ProducerApiApplication {
 
     public static void main(String[] args) {
---
+++ producer-api/src/main/java/com/example/producer/core/props/KafkaProperties.java
@@ -0,0 +1,23 @@
+package com.example.producer.core.props;
+
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+
+@Slf4j
+@Getter
+@ToString
+@ConfigurationProperties("kafka.config")
+public class KafkaProperties {
+
+    private final String ip;
+    private final String port;
+
+    @ConstructorBinding
+    public KafkaProperties(String ip, String port) {
+        this.ip = ip;
+        this.port = port;
+    }
+}
