fbe161eba6ca008d4fba09aa56d444c0b933742c
SeokRae <kslbsh@gmail.com>
2023-08-13 15:19:12 +0900

    - [x] 카프카 토픽 정보 조회 추가

--- admin-api/src/main/java/com/example/admin/AdminApiApplication.java
+++ admin-api/src/main/java/com/example/admin/AdminApiApplication.java
@@ -3,8 +3,10 @@ package com.example.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.example.admin.core.props")
 public class AdminApiApplication {
 
     public static void main(String[] args) {
--- admin-api/src/main/java/com/example/admin/topic/KafkaTopicAdmin.java
+++ admin-api/src/main/java/com/example/admin/topic/KafkaTopicAdmin.java
@@ -1,47 +1,52 @@
-package com.example.admin.broker;
+package com.example.admin.topic;
 
 import com.example.admin.core.props.KafkaProperties;
+import com.example.admin.core.props.TopicProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.DescribeConfigsResult;
+import org.apache.kafka.clients.admin.TopicDescription;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.Node;
 import org.apache.kafka.common.config.ConfigResource;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @Component
-public class KafkaBrokerAdmin {
+public class KafkaTopicAdmin {
 
     private final AdminClient admin;
 
-    public KafkaBrokerAdmin(KafkaProperties kafkaProperties) {
+    public KafkaTopicAdmin(KafkaProperties kafkaProperties, TopicProperties topicProperties) {
         Properties configs = new Properties();
 
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getDomain());
 
         this.admin = AdminClient.create(configs);
         log.info("kafka admin adminClient : {}", admin);
-        retrieveBroker();
+        retrieveTopics(topicProperties.getTopic());
     }
 
-    public void retrieveBroker() {
-        log.info("Get Broker Information");
+    public void retrieveTopics(String topic) {
+        log.info("Get Topic Information");
 
         try {
             for(Node node : admin.describeCluster().nodes().get()) {
                 log.info("node : {}", node);
 
-                ConfigResource configResource = new ConfigResource(ConfigResource.Type.BROKER, node.idString());
-                DescribeConfigsResult describeConfigsResult = admin.describeConfigs(Collections.singleton(configResource));
-
-                describeConfigsResult.all().get().forEach((configResource1, config) -> {
-                    config.entries().forEach(entry -> log.info("entry :: [{}]: [{}]", entry.name(), entry.value()));
-                });
+                Map<String, TopicDescription> topicInformation = admin.describeTopics(Collections.singletonList(topic))
+                        .allTopicNames() /* 3.1.0 버전 이후로 all() 대신 allTopicNames() 사용 */
+                        .get();
+                topicInformation.forEach(
+                        (key, value) -> {
+                            log.info("key : {}, value : {}", key, value);
+                            log.info("topic : {}, partition : {}, replicas : {}", value.name(), value.partitions().size(), value.partitions().get(0).replicas().size());
+                        });
             }
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
